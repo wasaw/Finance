f5b7276396a95044598505a3e4851c1500aa2508
wasaw <merenkovalexander@gmail.com>
2022-06-14 16:04:00 +0300

    Создан внешний вид TabBarController

--- Finance/Controller/TabBarController.swift
+++ Finance/Controller/TabBarController.swift
@@ -11,10 +11,23 @@ class TabBarController: UITabBarController {
     
 //    MARK: - Lifecycle
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let lessWidth: CGFloat = 20
+        let lessHeight: CGFloat = 15
+        tabBar.frame.size.width = view.frame.width - lessWidth
+        tabBar.frame.size.height = tabBar.frame.size.height - lessHeight
+        tabBar.frame.origin.y = tabBar.frame.origin.y - lessHeight / 2
+        tabBar.frame.origin.x = lessWidth / 2
+        tabBar.layer.cornerRadius = lessWidth
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tabBar.backgroundColor = .white
+                
+        tabBar.backgroundColor = .tabBarBackgroundColor
+        tabBar.tintColor = .totalAccountBackground
+        tabBar.barTintColor = .backgroundColor
         
         let homeVC = UINavigationController(rootViewController: HomeController())
         homeVC.tabBarItem.image = UIImage(systemName: "house.fill")
--- Finance/Utils/Extensions.swift
+++ Finance/Utils/Extensions.swift
@@ -13,5 +13,6 @@ extension UIColor {
     static let totalAccountTintColor = UIColor(displayP3Red: 0, green: 53/255, blue: 102/255, alpha: 1)
     static let totalAccountBackground = UIColor(displayP3Red: 255/255, green: 191/255, blue: 0, alpha: 1)
     static let servicesCellImageBackground = UIColor(displayP3Red: 228/255, green: 252/255, blue: 255/255, alpha: 1)
+    static let tabBarBackgroundColor = UIColor(displayP3Red: 26/255, green: 35/255, blue: 75/255, alpha: 1)
     static let backgroundColor = UIColor(displayP3Red: 233/255, green: 236/255, blue: 239/255, alpha: 1)
 }
